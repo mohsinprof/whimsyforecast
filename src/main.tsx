22067624c62294b37e1bd3e51c7f40c14abf096b
mohsinprof <mohsin.professional.job@gmail.com>
2026-02-17 15:45:02 +0500

    Reset project to single page

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,16 @@
-import {createRoot} from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./App.css";
 
-createRoot(document.getElementById('root')!).render(
-    <App/>
-)
+const root = document.getElementById("root");
+
+if (!root) {
+	throw new Error("Root element not found");
+}
+
+createRoot(root).render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
